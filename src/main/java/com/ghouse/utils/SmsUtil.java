c77f75de94975098761910a2221d4f41e5299f59
godlikehzj <godlikehzj@qq.com>
2017-01-06 19:05:07 +0800

    modify sms

---
+++ src/main/java/com/ghouse/utils/SmsUtil.java
@@ -0,0 +1,37 @@
+package com.ghouse.utils;
+
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
+import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
+
+/**
+ * Created by zhijunhu on 2017/1/6.
+ */
+public class SmsUtil {
+    private static String url = "http://gw.api.taobao.com/router/rest";
+    private static String key = "23592884";
+    private static String secret = "3a251c93d550f894837031eb0e37efdc";
+    private static String signName = "虹巢环保test";
+    private static String templateCode = "SMS_39045012";
+    private static TaobaoClient client = new DefaultTaobaoClient(url, key, secret);
+
+    public static boolean sendMessege(String code, String mobile){
+        AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
+        req.setExtend("123456");
+        req.setSmsType("normal");
+        req.setSmsFreeSignName(signName);
+        req.setSmsParamString("{\"code\":\""+code+"\"}");
+        req.setRecNum(mobile);
+        req.setSmsTemplateCode(templateCode);
+        try{
+            AlibabaAliqinFcSmsNumSendResponse rsp = client.execute(req);
+            System.out.println(rsp.getBody());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
--- src/main/java/com/ghouse/utils/TestUtil.java
+++ src/main/java/com/ghouse/utils/TestUtil.java
@@ -1,6 +1,10 @@
 package com.ghouse.utils;
 
 import com.ghouse.bean.HouseInfo;
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
+import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
 
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
@@ -10,8 +14,21 @@ import java.lang.reflect.Method;
  * Created by godlikehzj on 2016/12/29.
  */
 public class TestUtil {
-    public static void main(String[] args){
-        String clientId = "1234";
-        System.out.print(clientId.substring(0, clientId.length() - 1));
+    public static void main (String[] args){
+        TaobaoClient client = new DefaultTaobaoClient("http://gw.api.taobao.com/router/rest", "23592884", "3a251c93d550f894837031eb0e37efdc");
+        AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
+        req.setExtend("123456");
+        req.setSmsType("normal");
+        req.setSmsFreeSignName("虹巢环保test");
+        req.setSmsParamString("{\"code\":\"1234\"}");
+        req.setRecNum("15810531937");
+        req.setSmsTemplateCode("SMS_39045012");
+        try{
+            AlibabaAliqinFcSmsNumSendResponse rsp = client.execute(req);
+            System.out.println(rsp.getBody());
+
+        }catch (Exception e){
+
+        }
     }
 }
