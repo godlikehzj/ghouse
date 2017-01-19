70bf05cccf80425ee2dbd4f6c5d9aaa4dac135be
godlikehzj <godlikehzj@qq.com>
2017-01-20 00:52:19 +0800

    modify

--- src/main/java/com/ghouse/utils/CommonUtil.java
+++ src/main/java/com/ghouse/utils/CommonUtil.java
@@ -2,6 +2,7 @@ package com.ghouse.utils;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.io.*;
 import java.util.Date;
 import java.util.Random;
 import java.util.regex.Matcher;
@@ -24,5 +25,26 @@ public class CommonUtil {
         String token = mobile + date.getTime() + random.nextInt(100000);
 
         return DigestUtils.md5Hex(token);
+    }
+
+    public static void io(Reader in, Writer out, int bufferSize) throws IOException {
+        if(bufferSize == -1) {
+            bufferSize = 4096;
+        }
+
+        char[] buffer = new char[bufferSize];
+
+        int amount;
+        while((amount = in.read(buffer)) >= 0) {
+            out.write(buffer, 0, amount);
+        }
+
+    }
+
+    public static String convertStreamToString(InputStream in) throws IOException{
+        InputStreamReader reader = new InputStreamReader(in);
+        StringWriter writer = new StringWriter();
+        io((Reader)reader, (Writer)writer, -1);
+        return writer.toString();
     }
 }
